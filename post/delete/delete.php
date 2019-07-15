02a0dde1b53b25ad3fb20e24f73a8f232e7e02c8
eduardbadalyan <eduard.badalyan00@gmail.com>
2019-07-15 20:05:20 +0400

    0

---
+++ post/delete/delete.php
@@ -0,0 +1,15 @@
+<?php
+    $mysqli = new mysqli ("localhost", "root", "root", "myBase");
+    $mysqli->query ("SET NAMES 'utf8'");
+    $result_set = $mysqli->query ("SELECT posts.*,users.name FROM `posts` INNER JOIN `users` ON users.id=posts.user_id ORDER BY posts.id");
+
+    while (($row = $result_set->fetch_assoc()) != false) {
+        if($_GET["delete"] == $row["id"]) {
+            $id = $row["id"];
+            $mysqli->query ("DELETE FROM `posts` WHERE `posts`.`id` =".$id.";");
+            $mysqli->close ();
+            header ("Location: /");
+            exit;
+        };
+    };
+?>
