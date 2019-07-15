e4ed1ddc04aeae243432b5c6a5680a6e200d1665
eduardbadalyan <eduard.badalyan00@gmail.com>
2019-07-15 15:36:53 +0400

    edit

--- edit.php
+++ edit.php
@@ -14,12 +14,9 @@
         if(isset($_POST["done"])) {
             $title = htmlspecialchars ($_POST["title"]);
             $description = htmlspecialchars ($_POST["description"]);
-            $user = htmlspecialchars ($_POST["users"]);
-            $user_id = $mysqli->query ("SELECT id FROM `users` WHERE name=\"$user\"")->fetch_assoc()["id"];
 
             $mysqli->query ("UPDATE `posts` SET `title` = '".$title."' WHERE `posts`.`id` = ".$id.";");
             $mysqli->query ("UPDATE `posts` SET `description` = '".$description."' WHERE `posts`.`id` = ".$id.";");
-            $mysqli->query ("UPDATE `posts` SET `user_id` = '".$user_id."' WHERE `posts`.`id` = ".$id.";");
             $mysqli->close ();
             header ("Location: index.php");
             exit;
@@ -41,27 +38,12 @@
         <h2>Edit post</h2>
         <form name="feed" action="" method="post">
             <label for="title"><h4>Title: </h4></label><br>
-            <input type="text" id="title" name="title" value="<?=$title?>" placeholder="add Title..."><br><br>
+            <input type="text" id="title" name="title" style="width:290px;padding:5px;" value="<?=$title?>" placeholder="add Title..."><br><br>
 
             <label for="description"><h4>Description: </h4></label><br>
             <textarea name="description" id="description" cols="30" rows="5" placeholder="Add description..."><?=$description?></textarea><br><br>
 
-            <label for=""><h6>Choose user: </h6></label>
-            <select name="users" id="users" sele>
-                <?php
-                    function printResult ($select_users,$user) {
-                        while (($row = $select_users->fetch_assoc()) != false) {
-                            if($row["name"] == $user){
-                                echo "<option value=\"".$row["name"]."\" selected>".$row["name"]."</option>";
-                            }
-                            else {
-                                echo "<option value=\"".$row["name"]."\">".$row["name"]."</option>";
-                            }
-                        }
-                    };
-                    printResult($select_users,$user);
-                ?>
-            </select><br><br>
+            <h6>User: <?=$user?></h6><br><br>
 
             <input type="submit" name="done" value="Done!" class="btn btn-success">
         </form>
