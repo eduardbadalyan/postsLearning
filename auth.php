3f238bcbb97a032f0c0ffce7d90ab573778642cf
eduardbadalyan <eduard.badalyan00@gmail.com>
2019-07-15 12:16:04 +0400

    auth

---
+++ auth.php
@@ -0,0 +1,20 @@
+<?php
+    $email = filter_var(trim($_POST["email"]),FILTER_SANITIZE_STRING);
+    $password = filter_var(trim($_POST["password"]),FILTER_SANITIZE_STRING);
+
+    $password = md5($password);
+
+    $mysqli = new mysqli("localhost", "root", "root", "myBase");
+    //$mysqli->query ("SET NAMES 'utf8'");
+    $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email' AND `password`='$password'");
+    $user = $result->fetch_assoc();
+    if(count($user) == 0) {
+        echo "This is invalid user!!!";
+        exit();
+    }
+
+    setcookie('user', $user['name'], time() + 3600 * 24, "/");
+
+    $mysqli->close ();
+    header ("Location: index.php");
+?>
