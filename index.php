437cbb5ee12b25e3e99eb2eb58dd91ecc9492e0e
eduardbadalyan <eduard.badalyan00@gmail.com>
2019-07-15 20:02:49 +0400

    0

--- index.php
+++ index.php
@@ -2,32 +2,28 @@
     $mysqli = new mysqli ("localhost", "root", "root", "myBase");
     $mysqli->query ("SET NAMES 'utf8'");
     $result_set = $mysqli->query ("SELECT posts.*,users.name FROM `posts` INNER JOIN `users` ON users.id=posts.user_id ORDER BY posts.id");
+    $result = $mysqli->query ("SELECT posts.*,users.name FROM `posts` INNER JOIN `users` ON users.id=posts.user_id ORDER BY posts.id");
     $mysqli->close ();
-
     
     if(isset($_POST["add_post"])) {
-        header ("Location: create.php?create=1");
+        header ("Location: post/create/");
         exit;
     };
-
     if(isset($_POST["register"])) {
-        header ("Location: register.php");
+        header ("Location: auth/register/");
         exit;
     };
-
     if(isset($_POST["exit"])) {
-        header ("Location: exit.php");
+        header ("Location: auth/exit/exit.php");
         exit;
     };
-
-    $count = $result_set->num_rows;
-    for($i=1 ; $i<= $count ; $i++) {
-        if(isset($_POST["edit_post".$i.""])) {
-            header ("Location: edit.php?edit=".$i."");
+    while (($line = $result->fetch_assoc()) != false) {
+        if(isset($_POST["edit_post".$line["id"].""])) {
+            header ("Location: post/edit/?edit=".$line["id"]."");
             exit;
         }
-        if(isset($_POST["delete_post".$i.""])) {
-            header ("Location: delete.php?delete=".$i."");
+        if(isset($_POST["delete_post".$line["id"].""])) {
+            header ("Location: post/delete/delete.php?delete=".$line["id"]."");
             exit;
         }
     };
@@ -38,25 +34,15 @@
         <link   rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
                                  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
                                  crossorigin="anonymous">
-        <link rel="stylesheet" href="style_index.css">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
             <div class="header dropdown-header table-dark">
-                <span class="btn title-header">.</span>
-                <span class="btn title-header" style="float: left;">Start Bootstrap</span>
-                <div class="row" style="float: left;">
+                <div class="row">
                 <?php
                     if($_COOKIE['name'] == ''):
                 ?>
-                <form name="login" action="auth.php" method="post" style="margin-block-end: 0;">
-                <label for="email">
-                    <span style="color:white;margin-left:20px;">Email : </span>
-                </label>
-                <input type="text" name="email" id="email" placeholder="enter your email" style="padding:2px 5px;">
-                <label for="password">
-                    <span style="color:white;margin-left:20px;">Password : </span>
-                </label>
-                <input type="password" name="password" id="password" placeholder="enter your password" style="padding:2px 5px;">
+                <form name="login" action="auth/login/" method="post" style="margin-block-end: 0;">
                 <input type="submit" name="login" value="Log in" class="btn btn-outline-light" style="margin-left:20px;">
                 </form>
                 <form action="" name="register" method="post" style="margin-block-end: 0;">
@@ -73,16 +59,22 @@
                     endif;
                 ?>
                 </div>
-                <span class="btn btn-dark btn-header">Contact</span>
-                <span class="btn btn-dark btn-header">Services</span>
-                <span class="btn btn-dark btn-header">About</span>
-                <span class="btn btn-dark btn-header">Home</span>
             </div>
         <div class="content">
             <div class="container-fluid">
                 <div class="container">
                     <form action="" name="feed" method="post">
                         <?php
+                            if($_COOKIE['name'] != ''):
+                        ?>
+                        <div class="card text-center card-container">
+                            <div class="card-body">
+                                <input type="submit" name="add_post" value="Add post" class="btn btn-primary" style="margin:auto;padding:10px;">
+                            </div>
+                        </div>
+                        <?php
+                            endif;
+                            
                             function printResult ($result_set) {
                                 while (($row = $result_set->fetch_assoc()) != false) {
                                     echo "  <div class=\"card text-justify card-container\">
@@ -104,15 +96,6 @@
                                 }
                             };
                             printResult($result_set);
-                            if($_COOKIE['name'] != ''):
-                        ?>
-                        <div class="card text-center card-container">
-                            <div class="card-body">
-                                <input type="submit" name="add_post" value="Add post" class="btn btn-primary" style="margin:auto;padding:10px;">
-                            </div>
-                        </div>
-                        <?php
-                            endif;
                         ?>
                     </form>
                 </div>
