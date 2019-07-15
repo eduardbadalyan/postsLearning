a86f5abd62b59f76c8b7cc69422735a9f634aa36
eduardbadalyan <eduard.badalyan00@gmail.com>
2019-07-15 20:04:50 +0400

    0

---
+++ post/create/index.php
@@ -0,0 +1,42 @@
+<?php
+        $mysqli = new mysqli ("localhost", "root", "root", "myBase");
+        $mysqli->query ("SET NAMES 'utf8'");
+        $select_users = $mysqli->query ("SELECT * FROM `users` ORDER BY id");
+        if(isset($_POST["done"])) {
+            $title = htmlspecialchars ($_POST["title"]);
+            $description = htmlspecialchars ($_POST["description"]);
+            $user_id = $_COOKIE['user_id'];
+
+            $mysqli->query ("INSERT INTO `posts` VALUES (NULL,'".$title."','".$description."','".$user_id."')");
+            $mysqli->close ();
+            header ("Location: /");
+            exit;
+        }
+?>
+<!DOCTYPE>
+<html>
+<head>
+    <title>Create post</title>
+    <link   rel="stylesheet" 
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+            crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="content">
+        <h2>Add post</h2>
+        <form name="feed" action="" method="post">
+            <label for="title"><h4>Title: </h4></label><br>
+            <input type="text" id="title" name="title" style="width:290px;padding:5px;" placeholder="add Title..."><br><br>
+
+            <label for="description"><h4>Description: </h4></label><br>
+            <textarea name="description" id="description" cols="30" rows="5" placeholder="Add description..."></textarea><br><br>
+
+            <h6>User: <?=$_COOKIE['name'];?></h6><br><br>
+
+            <input type="submit" name="done" value="Done!" class="btn btn-success">
+        </form>
+    </div>
+</body>
+</html>
