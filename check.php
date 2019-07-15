bb0e802831ca48e9ca350250f061c0ec44de0f91
eduardbadalyan <eduard.badalyan00@gmail.com>
2019-07-15 12:16:22 +0400

    check

---
+++ check.php
@@ -0,0 +1,23 @@
+<?php
+    $name = filter_var(trim($_POST["name"]),FILTER_SANITIZE_STRING);
+    $password = filter_var(trim($_POST["password"]),FILTER_SANITIZE_STRING);
+    $checkPassword = filter_var(trim($_POST["password"]),FILTER_SANITIZE_STRING);
+    $email = filter_var(trim($_POST["email"]),FILTER_SANITIZE_STRING);
+    $age = filter_var(trim($_POST["age"]),FILTER_SANITIZE_NUMBER_INT);
+
+    $age = intval($age);
+    $checkPassword = md5($checkPassword);
+    $password = md5($password);
+
+    if(strlen($name) == 0 || $email == "" || !preg_match ("/@/", $email) || strlen($password) == 0 || $password != $checkPassword  ||$age == 0 || $age == "") {
+        header ("Location: register.php?error=1");
+        exit();
+    };
+
+    $mysqli = new mysqli("localhost", "root", "root", "myBase");
+    $result = $mysqli->query("INSERT INTO `users` VALUES (NULL,'".$name."','".$age."','".$email."','".$password."')");
+
+    $mysqli->close ();
+    header ("Location: index.php");
+    exit();
+?>
