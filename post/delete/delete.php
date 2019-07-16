7b45ed84d7e3fb2b9d3631a3ae1bbe111a1e2d9d
eduardbadalyan <eduard.badalyan00@gmail.com>
2019-07-16 14:50:43 +0400

    0

--- post/delete/delete.php
+++ post/delete/delete.php
@@ -2,7 +2,12 @@
     $mysqli = new mysqli ("localhost", "root", "root", "myBase");
     $mysqli->query ("SET NAMES 'utf8'");
     $result_set = $mysqli->query ("SELECT posts.*,users.name FROM `posts` INNER JOIN `users` ON users.id=posts.user_id ORDER BY posts.id");
-
+    if($_COOKIE['name'] == ''){
+        $mysqli->close ();
+        header ("Location: /");
+        exit;
+    }
+    else{
     while (($row = $result_set->fetch_assoc()) != false) {
         if($_GET["delete"] == $row["id"]) {
             $id = $row["id"];
@@ -11,5 +16,6 @@
             header ("Location: /");
             exit;
         };
+    };
     };
 ?>
