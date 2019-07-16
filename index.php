5dbcabd60641b5302a356a2a93da212081965100
eduardbadalyan <eduard.badalyan00@gmail.com>
2019-07-16 13:48:34 +0400

    0

--- index.php
+++ index.php
@@ -27,6 +27,14 @@
             exit;
         }
     };
+    if(isset($_POST["home"])) {
+        header ("Location: /");
+        exit;
+    };
+    if(isset($_POST["myPosts"])) {
+        header ("Location: /?my-posts=1");
+        exit;
+    };
 ?>
 <html>
     <head>
@@ -42,18 +50,18 @@
                 <?php
                     if($_COOKIE['name'] == ''):
                 ?>
-                <form name="login" action="auth/login/" method="post" style="margin-block-end: 0;">
-                <input type="submit" name="login" value="Log in" class="btn btn-outline-light" style="margin-left:20px;">
+                <form name="login" action="auth/login/" method="post">
+                <input type="submit" name="login" value="Log in" class="btn btn-outline-light btn-header">
                 </form>
-                <form action="" name="register" method="post" style="margin-block-end: 0;">
-                <input type="submit" name="register" value="Register" class="btn btn-outline-light" style="margin-left:20px;">
+                <form action="" name="register" method="post">
+                <input type="submit" name="register" value="Register" class="btn btn-outline-light btn-header">
                 </form>
                 <?php
                     else:
                 ?>
                 <form action="" name="exit" method="post">
                 <span style="color:white;margin-left:20px;">Hello <?=$_COOKIE['name']?>!!!</span>
-                <input type="submit" name="exit" value="Exit" class="btn btn-outline-light" style="margin-left:20px;">
+                <input type="submit" name="exit" value="Exit" class="btn btn-outline-light btn-header"">
                 </form>
                 <?php
                     endif;
@@ -61,6 +69,16 @@
                 </div>
             </div>
         <div class="content">
+        <?php
+            if($_COOKIE['name'] != ''):
+        ?>
+            <form action="" name="pages" method="post" style="position:absolute;right:45%;">
+            <input type="submit" name="home" value="Home" class="btn btn-secondary btn-pages">|
+            <input type="submit" name="myPosts" value="My Posts" class="btn btn-secondary btn-pages">
+            </form><br><br>
+        <?php
+            endif;
+        ?>
             <div class="container-fluid">
                 <div class="container">
                     <form action="" name="feed" method="post">
@@ -74,7 +92,23 @@
                         </div>
                         <?php
                             endif;
-                            
+                            if($_GET["my-posts"] == 1){
+                                function printMyPosts ($result_set) {
+                                    while (($row = $result_set->fetch_assoc()) != false) {
+                                        if ($_COOKIE['user_id'] == $row["user_id"]){
+                                        echo "  <div class=\"card text-justify card-container\">
+                                                    <div class=\"card-body\">                                                 
+                                                        <h4 class=\"card-title\">".$row["title"]."</h4>
+                                                        <p class=\"card-text\">".$row["description"]."</p>
+                                                        <p class=\"card-text text-right\" style=\"font-size:12px;\">".$row["name"]."</p>
+                                                        <input type=\"submit\" name=\"edit_post".$row["id"]."\" value=\"Edit post\" class=\"btn btn-success\">
+                                                        <input type=\"submit\" name=\"delete_post".$row["id"]."\" value=\"Delete post\" class=\"btn btn-danger\">
+                                                        </div></div>";
+                                                };
+                                    }
+                                };
+                                printMyPosts($result_set);
+                            }else{
                             function printResult ($result_set) {
                                 while (($row = $result_set->fetch_assoc()) != false) {
                                     echo "  <div class=\"card text-justify card-container\">
@@ -95,7 +129,7 @@
                                             };
                                 }
                             };
-                            printResult($result_set);
+                            printResult($result_set);};
                         ?>
                     </form>
                 </div>
