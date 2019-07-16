8be32951c3840955aed3d535924278bd5a0a73ca
eduardbadalyan <eduard.badalyan00@gmail.com>
2019-07-16 15:16:36 +0400

    0

--- post/delete/delete.php
+++ post/delete/delete.php
@@ -2,20 +2,21 @@
     $mysqli = new mysqli ("localhost", "root", "root", "myBase");
     $mysqli->query ("SET NAMES 'utf8'");
     $result_set = $mysqli->query ("SELECT posts.*,users.name FROM `posts` INNER JOIN `users` ON users.id=posts.user_id ORDER BY posts.id");
-    if($_COOKIE['name'] == ''){
+    while (($row = $result_set->fetch_assoc()) != false) {
+        if($_GET["delete"] == $row["id"]) {
+            $id = $row["id"];
+            $user_id = $row["user_id"];
+        };
+    };   
+    if($_COOKIE['user_id'] != $user_id){
         $mysqli->close ();
         header ("Location: /");
         exit;
     }
     else{
-    while (($row = $result_set->fetch_assoc()) != false) {
-        if($_GET["delete"] == $row["id"]) {
-            $id = $row["id"];
             $mysqli->query ("DELETE FROM `posts` WHERE `posts`.`id` =".$id.";");
             $mysqli->close ();
             header ("Location: /");
             exit;
         };
-    };
-    };
 ?>
